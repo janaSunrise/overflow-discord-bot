043034ce8afd5ea40fda7e061b52bb096ba90396
janaSunrise <warriordefenderz@gmail.com>
2021-02-08 23:25:18 +0530

    Start working on basic commands

--- bot/cogs/fun/fun.py
+++ bot/cogs/fun/fun.py
@@ -1,3 +1,6 @@
+import random
+
+import discord
 from discord.ext import commands
 
 from bot import Bot
@@ -7,3 +10,30 @@ class Fun(commands.Cog):
     """A cog designed for fun based commands."""
     def __init__(self, bot: Bot):
         self.bot = bot
+
+    @commands.command()
+    async def catfact(self, ctx: commands.Context) -> None:
+        async with self.bot.session.get("https://cat-fact.herokuapp.com/facts") as response:
+            self.all_facts = await response.json()
+
+        fact = random.choice(self.all_facts["all"])
+        await ctx.send(
+            embed=discord.Embed(
+                title="Did you Know?",
+                description=fact["text"],
+                color=discord.Color.blurple()
+            )
+        )
+
+    @commands.command()
+    async def chuck(self, ctx: commands.Context) -> None:
+        """Get a random Chuck Norris joke."""
+        if random.randint(0, 1):
+            async with self.bot.session.get("https://api.chucknorris.io/jokes/random") as r:
+                joke = await r.json()
+                await ctx.send(joke["value"])
+                return
+
+        async with self.bot.session.get("http://api.icndb.com/jokes/random") as response:
+            joke = await response.json()
+            await ctx.send(joke["value"]["joke"].replace("&quote", '"'))
