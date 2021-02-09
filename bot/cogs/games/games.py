6e168f8a1d45b5bb6a280d920f6c6e69a6091fd5
janaSunrise <warriordefenderz@gmail.com>
2021-02-09 13:40:05 +0530

    Add 8ball and hangman

--- bot/cogs/games/games.py
+++ bot/cogs/games/games.py
@@ -1,7 +1,10 @@
+import random
+
 import discord
 from discord.ext import commands
 
-from bot import Bot
+from bot import Bot, config
+from .hangman import HangmanGame
 from .tic_tac_toe import TTT_Game
 
 
@@ -9,8 +12,32 @@ class Games(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
+    @commands.command(aliases=["8ball"])
+    async def ball8(self, ctx: commands.Context, *, question: str) -> None:
+        """Ask the all-knowing 8ball your burning questions."""
+        reply_type = random.randint(1, 3)
+
+        if reply_type == 1:
+            answer = random.choice(config.BALL_REPLIES["positive"])
+        elif reply_type == 2:
+            answer = random.choice(config.BALL_REPLIES["negative"])
+        elif reply_type == 3:
+            answer = random.choice(config.BALL_REPLIES["error"])
+
+        embed = discord.Embed(title="Magic 8-ball", color=discord.Color.blurple())
+        embed.add_field(name="Question", value=question)
+        embed.add_field(name="Answer", value=answer)
+
+        await ctx.send(embed=embed)
+
     @commands.command(aliases=["ttt", "tictactoe"])
     async def tic_tac_toe(self, ctx: commands.Context, opponent: discord.Member = None) -> None:
         """Play a game of Tic-Tac-Toe."""
         game = TTT_Game(ctx.author, opponent, clear_reactions_after=True)
         await game.start(ctx)
+
+    @commands.command()
+    async def hangman(self, ctx: commands.Context) -> None:
+        """Play game of Hangman."""
+        hangman_game = HangmanGame.random(ctx)
+        await hangman_game.play()
--- bot/config.py
+++ bot/config.py
@@ -24,6 +24,60 @@ nodes = {
 spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
 spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
+# -- 8ball response --
+BALL_REPLIES = {
+    "positive": [
+        "Yep.",
+        "Absolutely!",
+        "Can do!",
+        "Affirmative!",
+        "Yeah okay.",
+        "Sure.",
+        "Sure thing!",
+        "You're the boss!",
+        "Okay.",
+        "No problem.",
+        "I got you.",
+        "Alright.",
+        "You got it!",
+        "ROGER THAT",
+        "Of course!",
+        "Aye aye, cap'n!",
+        "I'll allow it.",
+    ],
+    "negative": [
+        "Noooooo!!",
+        "Nope.",
+        "I'm sorry Dave, I'm afraid I can't do that.",
+        "I don't think so.",
+        "Not gonna happen.",
+        "Out of the question.",
+        "Huh? No.",
+        "Nah.",
+        "Naw.",
+        "Not likely.",
+        "No way, José.",
+        "Not in a million years.",
+        "Fat chance.",
+        "Certainly not.",
+        "NEGATORY.",
+        "Nuh-uh.",
+        "Not in my house!",
+    ],
+    "error": [
+        "Please don't do that.",
+        "You have to stop.",
+        "Do you mind?",
+        "In the future, don't do that.",
+        "That was a mistake.",
+        "You blew it.",
+        "You're bad at computers.",
+        "Are you trying to kill me?",
+        "Noooooo!!",
+        "I can't believe you've done this",
+    ]
+}
+
 # -- Search --
 basic_search_categories = [
     "web",
