84b5cb7a2d1f2da7b954c334478899d47f594aed
janaSunrise <warriordefenderz@gmail.com>
2021-02-12 16:43:54 +0530

    Create the moderation embed.

--- bot/utils/embeds.py
+++ bot/utils/embeds.py
@@ -1,6 +1,29 @@
+from datetime import datetime
+from textwrap import dedent
+
 import discord
+from discord.ext import commands
 
 
-def moderation_embed(description: str, color: discord.Color = discord.Color.blue()) -> discord.Embed:
+def moderation_embed(
+        ctx: commands.Context,
+        action: str,
+        user: discord.Member,
+        reason: str,
+        color: discord.Color = discord.Color.blue()
+) -> discord.Embed:
     """Return a quick embed for moderation event."""
-    return discord.Embed(description=description, color=color)
+    description = dedent(f"""
+    User: {user} [`{user.id}`]
+    Reason Specified: {reason}
+    
+    Moderator: {ctx.author} [`{ctx.author.id}`]
+    """)
+
+    embed = discord.Embed(
+        title=f"User {action}",
+        description=description,
+        color=color,
+        timestamp=datetime.utcnow()
+    )
+    return embed
