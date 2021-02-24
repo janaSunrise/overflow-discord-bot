b0aed69a7891d6884b878a77e258343bac6abae7
janaSunrise <warriordefenderz@gmail.com>
2021-02-24 15:27:33 +0530

    Work on announcement publishing.

--- bot/cogs/utility/announcements.py
+++ bot/cogs/utility/announcements.py
@@ -9,6 +9,11 @@ class Announcements(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
+    @property
+    def embeds_cog(self) -> discord.Embed:
+        """Get currently loaded Embed cog instance."""
+        return self.bot.get_cog("Embeds")
+
     @group(invoke_without_command=True)
     async def announcement(self, ctx: Context) -> None:
         """Commands for hackernews and it's feeds."""
@@ -24,7 +29,18 @@ class Announcements(Cog):
         await AnnouncementDB.set_announcement_role(self.bot.database, ctx.guild.id, role)
 
         role = ctx.guild.get_role(role)
-        await ctx.send(f"The role has been Successfully to {role.mention}")
+        await ctx.send(f"The role has been successfully set to {role.mention}")
+
+    @announcement.command()
+    @has_permissions(manage_channels=True)
+    async def channel(self, ctx: Context, channel: discord.TextChannel) -> None:
+        """Setup the announcement role."""
+        if isinstance(channel, discord.Role):
+            channel = channel.id
+
+        await AnnouncementDB.set_announcement_channel(self.bot.database, ctx.guild.id, channel)
+
+        await ctx.send(f"The channel has been successfully set to {channel.mention}")
 
     @announcement.command()
     async def subscribe(self, ctx: Context) -> None:
--- bot/databases/announcements.py
+++ bot/databases/announcements.py
@@ -12,6 +12,7 @@ class Announcements(DatabaseBase):
     __tablename__ = "announcements"
 
     guild_id = Column(BigInteger, primary_key=True, nullable=False, unique=True)
+    channel_id = Column(BigInteger, nullable=False)
     role_id = Column(BigInteger, nullable=False)
 
     @classmethod
@@ -29,11 +30,25 @@ class Announcements(DatabaseBase):
             return row.dict()
 
     @classmethod
+    async def get_announcement_channel(
+            cls, session: AsyncSession, guild_id: t.Union[str, int, discord.Guild]
+    ) -> t.Optional[dict]:
+        guild_id = get_datatype_int(guild_id)
+
+        try:
+            row = await session.run_sync(lambda session: session.query(cls).filter_by(guild_id=guild_id).first())
+        except NoResultFound:
+            return None
+
+        if row is not None:
+            return row.dict()
+
+    @classmethod
     async def set_announcement_role(
             cls,
             session: AsyncSession,
             guild_id: t.Union[str, int, discord.Guild],
-            role_id: t.Union[str, int, discord.TextChannel]
+            role_id: t.Union[str, int, discord.Role]
     ) -> None:
         guild_id = get_datatype_int(guild_id)
         role_id = get_datatype_int(role_id)
@@ -44,6 +59,26 @@ class Announcements(DatabaseBase):
             values={
                 "guild_id": guild_id,
                 "role_id": role_id
+            }
+        )
+        await session.commit()
+
+    @classmethod
+    async def set_announcement_role(
+            cls,
+            session: AsyncSession,
+            guild_id: t.Union[str, int, discord.Guild],
+            channel_id: t.Union[str, int, discord.TextChannel]
+    ) -> None:
+        guild_id = get_datatype_int(guild_id)
+        channel_id = get_datatype_int(channel_id)
+
+        await on_conflict(
+            session, cls,
+            conflict_columns=["guild_id"],
+            values={
+                "guild_id": guild_id,
+                "channel_id": channel_id
             }
         )
         await session.commit()
