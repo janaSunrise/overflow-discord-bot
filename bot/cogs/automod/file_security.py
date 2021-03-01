cc96c0dfd998dd2820e9a755723ad2d05ded7931
janaSunrise <warriordefenderz@gmail.com>
2021-03-01 16:07:50 +0530

    Add file security pastes.

---
+++ bot/cogs/automod/file_security.py
@@ -0,0 +1,112 @@
+import json
+import typing as t
+from contextlib import suppress
+from os.path import splitext
+
+import discord
+from discord.ext.commands import Cog
+from loguru import logger
+
+from bot import Bot
+
+FILE_EMBED_DESCRIPTION = (
+    f"""
+    Woops, your message got zapped by our spam filter.
+    We currently don't allow binary / unknown attachments or any source files, so here are some tips you can use:
+
+    • Try shortening your message, if it exceeds 2000 character limit
+    to fit within the character limit or use a pasting service (see below)
+    
+    • To protect other users, we keep a watch on the binaries, and the executable files sent and more, in order
+    to save them from malicious / harmful files.
+
+    • If you're showing code, you can use codeblocks or use a pasting service like [mystb.in](https://mystb.in) or [paste.gg](https://paste.gg)
+    """
+)
+
+WHITELIST_TYPES = (
+    ".3gp", ".3g2", ".avi", ".bmp", ".m4v", ".mkv",
+    ".mov", ".mp4", ".mpg", ".wmv", ".gif", ".jpg",
+    ".jpeg", ".png", ".svg", ".psd", ".ai", ".aep",
+    ".mp3", ".wav", ".ogg"
+)
+
+
+class FileSecurity(Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    async def file_uploader(self, attachments: list) -> t.Optional[str]:
+        file_list_json = []
+
+        for attachment in attachments:
+            try:
+                content = await attachment.read()
+                value = content.decode("utf-8")
+            except (discord.NotFound, ConnectionError):
+                continue
+
+            file_list_json.append({
+                "name": attachment.filename,
+                "content": {"format": "text", "value": value}
+            })
+
+        if len(file_list_json) == 0:
+            return
+
+        payload = {
+            "name": "Overflow paste service.",
+            "description": "Overflow file sec cog.",
+            "files": file_list_json
+        }
+
+        with suppress(discord.NotFound, ConnectionError):
+            response = await self.bot.session.post(
+                "https://api.paste.gg/v1/pastes",
+                headers={"Content-Type": "application/json"},
+                data=json.dumps(payload)
+            )
+
+        if response.status != 201:
+            return
+
+        key = (await response.json())["result"]["id"]
+        return f"https://www.paste.gg/{key}"
+
+    @Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        if not message.attachments or not message.guild:
+            return
+
+        if message.author.permissions_in(message.channel).manage_messages:
+            return
+        
+        attachments = []
+        for attachment in message.attachments:
+            extension = splitext(attachment.filename.lower())[1]
+
+            if extension in WHITELIST_TYPES:
+                continue
+            attachments.append(attachment)
+
+        if len(attachments) == 0:
+            return
+
+        logger.info(f"User <@{message.author.id}> posted a message on {message.guild.id} with protected "
+                    "attachments.")
+        embed = discord.Embed(description=FILE_EMBED_DESCRIPTION, color=discord.Color.dark_blue())
+
+        with suppress(discord.NotFound, ConnectionError):
+            await message.delete()
+            await message.channel.send(f"Hey {message.author.mention}!", embed=embed)
+
+            file_pastes = await self.file_uploader(attachments)
+
+            if file_pastes is not None:
+                paste_embed = discord.Embed(
+                    color=discord.Color.gold(),
+                    description=f"We uploaded the paste version of the files for you. The Paste(s) of the file(s) can "
+                                f"be found at {file_pastes}",
+                    title="Auto File Pastes!"
+                )
+                await message.channel.send(embed=paste_embed)
