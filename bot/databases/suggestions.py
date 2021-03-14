0f17cb2352c3975f543f5ae8e5639bb5e3948ac3
janaSunrise <warriordefenderz@gmail.com>
2021-03-14 14:36:53 +0530

    Edit the suggestion table a bit.

--- bot/databases/suggestions.py
+++ bot/databases/suggestions.py
@@ -7,31 +7,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from bot.databases import DatabaseBase, get_datatype_int
 
 
-class SuggestionID(DatabaseBase):
-    __tablename__ = "suggestion_id"
-
-    guild_id = Column(BigInteger, primary_key=True,
-                      nullable=False, unique=True)
-    next_id = Column(Integer, default=0)
-
-    @classmethod
-    async def get_next_id(
-        cls, session: AsyncSession, guild: t.Union[int, Guild]
-    ) -> int:
-        guild_id = get_datatype_int(guild)
-
-        row = await session.run_sync(
-            lambda session: session.query(
-                cls).filter_by(guild_id=guild_id).first()
-        )
-
-        row.next_id += 1
-        next_id = row.next_id
-
-        await session.commit()
-        return next_id
-
-
 class SuggestionConfig(DatabaseBase):
     __tablename__ = "suggestion_config"
 
@@ -51,7 +26,7 @@ class SuggestionUser(DatabaseBase):
 class Suggestion(DatabaseBase):
     __tablename__ = "suggestion"
 
-    suggestion_id = Column(BigInteger, primary_key=True, nullable=False)
+    suggestion_id = Column(BigInteger, primary_key=True, nullable=False, unique=True, autoincrement=True)
     guild_id = Column(BigInteger, primary_key=True,
                       nullable=False, unique=True)
     user_id = Column(BigInteger, nullable=False, unique=True)
