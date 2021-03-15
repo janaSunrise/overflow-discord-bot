818a136e41584a72cf6171cdc08f2f57f89238b9
janaSunrise <warriordefenderz@gmail.com>
2021-03-15 13:01:48 +0530

    Get invite code methods ready.

--- bot/databases/link_lock.py
+++ bot/databases/link_lock.py
@@ -21,3 +21,43 @@ class LinkLock(DatabaseBase):
     guild_id = Column(BigInteger, primary_key=True,
                       nullable=False, unique=True)
     lock_code = Column(Integer, nullable=False)
+
+    @classmethod
+    async def get_config(
+            cls, session: AsyncSession, guild_id: t.Union[str, int, discord.Guild]
+    ) -> t.Optional[dict]:
+        guild_id = get_datatype_int(guild_id)
+
+        try:
+            row = await session.run_sync(
+                lambda session_: session_.query(cls)
+                    .filter_by(guild_id=guild_id)
+                    .first()
+            )
+        except NoResultFound:
+            return None
+
+        if row is not None:
+            return row.dict()
+
+    @classmethod
+    async def set_lock(
+            cls,
+            session: AsyncSession,
+            guild_id: t.Union[str, int, discord.Guild],
+            lock_code: int,
+    ) -> None:
+        guild_id = get_datatype_int(guild_id)
+
+        await on_conflict(
+            session,
+            cls,
+            conflict_columns=["guild_id"],
+            values={"guild_id": guild_id, "lock_code": lock_code},
+        )
+        await session.commit()
+
+    def dict(self) -> t.Dict[str, t.Any]:
+        data = {key: getattr(self, key, None)
+                for key in self.__table__.columns.keys()}
+        return data
