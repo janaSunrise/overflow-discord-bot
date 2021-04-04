31761e3915177c06becf5825b728fd3a77145409
janaSunrise <warriordefenderz@gmail.com>
2021-04-04 14:15:01 +0530

    Added member update log.

--- bot/cogs/logging/member_log.py
+++ bot/cogs/logging/member_log.py
@@ -6,6 +6,7 @@ from discord.ext.commands import Cog
 
 from bot import Bot
 from bot.databases.logging import Logging
+from bot.utils.audit_log import get_latest_audit
 
 
 class MemberLog(Cog):
@@ -50,6 +51,35 @@ class MemberLog(Cog):
                 ),
                 color=discord.Color.blue()
             )
+        elif before.roles != after.roles:
+            new_roles = after.roles
+            old_roles = before.roles
+
+            stats = {
+                "added": old_roles - new_roles,
+                "removed": new_roles - old_roles
+            }
+
+            audit_log = await get_latest_audit(after.guild, [discord.AuditLogAction.member_role_update], after)
+
+            if audit_log:
+                user = audit_log.user
+
+            action = "added" if not stats.get("removed") else "removed"
+
+            description = textwrap.dedent(f"""
+            **`Mention`**: {after.mention}
+            **`Roles`**: {stats['removed'] if stats['removed'] else stats['added']}
+            """)
+
+            if audit_log:
+                description += f"\n**`Moderator`**: {user.mention}"
+
+            embed = discord.Embed(
+                title="Roles change",
+                description=description,
+                color=discord.Color.blue()
+            )
 
         embed.timestamp = datetime.utcnow()
         embed.set_thumbnail(url=after.avatar_url)
@@ -71,6 +101,41 @@ class MemberLog(Cog):
 
         for channel in member_logs:
             await channel.send(embed=embed)
+
+    @Cog.listener("on_member_update")
+    async def log_member_update(self, before: discord.Member, after: discord.Member) -> None:
+        if before.nick != after.nick:
+            embed = discord.Embed(
+                title="Nickname change",
+                description=textwrap.dedent(
+                    f"""
+                    **`Mention`**: {after.mention}
+                    **`Old nickname`**: {before.nick}
+                    **`New nickname`**: {after.nick}
+                    """
+                ),
+                color=discord.Color.blue()
+            )
+        elif before.pending != after.pending:
+            embed = discord.Embed(
+                title="Member verification completed",
+                description=textwrap.dedent(
+                    f"""
+                    **`Mention`**: {after.mention}]
+                    
+                    User has passed the server verification.
+                    """
+                ),
+                color=discord.Color.blue()
+            )
+        else:
+            return
+
+        embed.timestamp = datetime.utcnow()
+        embed.set_thumbnail(url=after.avatar_url)
+        embed.set_footer(text=f"ID: {after.id}")
+
+        await self.send_member_log(after.guild, embed=embed)
 
     async def send_member_log(self, guild: discord.Guild, *args, **kwargs) -> None:
         member_log_channel_id = await Logging.get_config(self.bot.database, guild.id)
